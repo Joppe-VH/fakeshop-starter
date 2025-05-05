cdc973d86626aa5721eaa4aed78aac3729ddbc78
Joppe-VH <joppe.vanhoof@gmail.com>
2025-05-05 12:08:33 +0200

    add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,14 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div className="flex h-screen flex-col">
       <NavBar />
-      <main className="flex-1 overflow-auto">{children}</main>
+      <div className="flex flex-1 flex-col gap-10 overflow-auto">
+        <main>{children}</main>
+        <footer className="mt-auto bg-gray-800 p-4 text-center text-white">
+          <div className="container mx-auto">
+            <p className="text-sm">&copy; {new Date().getFullYear()} Fake Shop. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
